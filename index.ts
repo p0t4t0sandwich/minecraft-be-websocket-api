3c13b9dbda893a51f02ec5520a213a30e89d4bcf
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-05-29 14:41:06 -0600

    Added types to event callbacks - made is so you can add event listeners from outside the class

--- index.ts
+++ index.ts
@@ -1,19 +1,28 @@
 import { WebSocketServer } from "ws";
-import { v4 as uuidv4 } from 'uuid';
 
 
 import { MinecraftWebSocket } from "./lib/MinecraftWebSocket.js";
+import { EventName, PlayerMessageEvent } from "./lib/Events.js";
 
 
 // Web Sockets
-let webSockets = {};
-let subscribeEvents = {};
-
 const WEBSOCKET_PORT: number = <number><unknown>process.env.WEBSOCKET_PORT || 4005;
 
 export const wss: WebSocketServer = new WebSocketServer({ port: WEBSOCKET_PORT }, () => {
     console.log(`MC BE Management Web Socket running on port ${WEBSOCKET_PORT}`);
 });
 
+
+// Minecraft Web Socket
 const mwss: MinecraftWebSocket = new MinecraftWebSocket(wss);
+
+await mwss.on(EventName.PlayerMessage, async (event: PlayerMessageEvent) => {
+    console.log("PlayerMessageEvent received: ", event);
+
+    // Ignore messages from the websocket server
+    if (event.body.sender == "Teacher") return
+
+    await mwss.sendCommand(event.server, `tellraw @a {"rawtext":[{"text":"${event.body.sender} said: ${event.body.message}"}]}`);
+});
+
 await mwss.start();
