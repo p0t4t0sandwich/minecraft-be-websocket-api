e1c498199f8fc1e5c179486231c2070cd0099cb1
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-06-03 15:43:02 -0600

    Some work on config stuffs and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./components/*.templ"],
+  content: ["./web/components/*.templ"],
   theme: {
     extend: {
       colors: {}
--- web/web.go
+++ web/web.go
@@ -7,6 +7,21 @@ import (
 	"github.com/p0t4t0sandwich/minecraft-be-websocket-api/web/components"
 )
 
+type config struct {
+	BannedMobs  []string `json:"banned_mobs"`
+	BannedItems []string `json:"banned_items"`
+}
+
+type WebServer struct {
+	Config *config
+}
+
+func NewWebServer(config *config) *WebServer {
+	return &WebServer{
+		Config: config,
+	}
+}
+
 func ApplyRoutes(router *http.ServeMux) *http.ServeMux {
 	router.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 
